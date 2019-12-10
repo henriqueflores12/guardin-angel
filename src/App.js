faaf52a2713c53bb7368c18f91bbd7b5c0e19047
fagner henrique flores <henriquefagner@hotmail.com>
2019-12-10 15:09:12 -0600

    commit for new changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+
+
+
+function showPosition(position){
+  console.log(position.coords.latitude)
+  console.log(position.coords.longitude)
+}
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>report crime or shooting by pushing the button down here</h3>
+        <button onClick={()=>{
+         if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(showPosition);
+        } else {
+          console.log("Geolocation is not supported by this browser")
+        }}}>report shooting</button>
+        <iframe
+          width="600"
+          height="450"
+          frameborder="0"
+          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJLwPMoJm1RIYRetVp1EtGm10&key=AIzaSyDsCYHfrvL3BCw_P2_8cydu1Eu16xV-BE8"
+          allowfullscreen
+        ></iframe>
+        
       </header>
     </div>
   );
