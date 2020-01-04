5f09134aa8c8c29ee5df815de0c5c924758bbbac
fagner henrique flores <henriquefagner@hotmail.com>
2020-01-04 13:07:26 -0600

    on hold

--- src/App.js
+++ src/App.js
@@ -1,54 +1,58 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import MapContainer from "./componets/MapContainer";
 
 
 
-
-function showPosition(position){
-  console.log(position.coords.latitude)
-  console.log(position.coords.longitude)
-
-}
-
-function App() {
+class App extends React.Component {
+  // state = {lat:"",lng:""
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3>report crime or shooting by pushing the button down here</h3>
-        <button onClick={()=>{
-         if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition);
-        } else {
-          console.log("Geolocation is not supported by this browser")
-        }}}>report shooting</button>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        {/* <iframe
-          width="600"
-          height="450"
-          frameborder="0"
-          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJLwPMoJm1RIYRetVp1EtGm10&"
-          allowfullscreen
-        ></iframe> */}
-        
-      </header>
-    </div>
-  );
+  // }
+  constructor (props){
+    super(props);
+  
+    this.state = {
+        lat: "",
+        lng: "",
+    };   
+  }
+  showPosition = (position) => {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+    console.log(this)
+    this.setState({
+      lat:position.coords.latitude,
+      lng:position.coords.longitude
+    })
+  }
+  
+  render(){
+    
+    return  (
+      <div className="App">
+        <header className="App-header">
+          <h3>report crime or shooting by pushing the button down here</h3>
+          <button
+            onClick={() => {
+              if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(this.showPosition);
+              } else {
+                console.log("Geolocation is not supported by this browser");
+              }
+            }}
+          >
+            report shooting
+          </button>
+  
+          <div className="map">
+            <MapContainer lat={this.state.lat} lng={this.state.lng}/>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/componets/MapContainer.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Map, Marker, GoogleApiWrapper, Circle } from "google-maps-react";
+export class MapContainer extends Component {
+  render() {
+    console.log(this.props, "props.mapcontainer");
+    console.log(Circle, "circle");
+    if (this.props.lat) {
+      console.log("if state", this.props.lat);
+      return (
+        <Map
+          className="Map"
+          google={this.props.google}
+          zoom={15}
+          center={{
+            lat: this.props.lat,
+            lng: this.props.lng
+          }}
+        >
+          {/* <Circle center={{lat: this.props.lat, lng: this.props.lng}} radius={0.5} strokeColor="#0000FF" strokeOpacity={0.2} strokeWeight={1} fillColor="#0000FF" fillOpacity={0.1} visible={true} draggable={true}/> */}
+          <Marker position={{ lat: this.props.lat, lng: this.props.lng }} />
+        </Map>
+      );
+    } else {
+      return (
+        <Map className="Map" google={this.props.google} zoom={8}>
+          <Marker onClick={this.onMarkerClick} name={"Current location"} />
+        </Map>
+      );
+    }
+  }
+}
+export default GoogleApiWrapper({
+  apiKey: ""
+})(MapContainer);
